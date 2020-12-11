c9a859a4e627f87fd6fdb0c73b373be2f438df3b
anish <anishsalgia1997@gmail.com>
2020-12-11 18:26:34 +0530

    Refactor: Added code for tie situation

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,27 +3,51 @@ echo "Welcome to FlipCoin Simulator"
 
 numOfHeads=0
 numOfTails=0
+margin=0
+
+function resultCounter ()
+{
+	if [ $1 -eq 0 ]
+		then
+			numOfHeads=$(( $numOfHeads + 1 ))
+		else
+			numOfTails=$(( $numOfTails + 1 ))
+		fi
+}
+
+function marginCheck ()
+{
+	if [ $1 -gt $2 ]
+	then
+		winMargin=$(( $1 - $2 ))
+	else
+		winMargin=$(( $2 - $1 ))
+	fi
+	echo $winMargin
+}
 
 echo "Welcome To Flip Coin Simulator"
 
-while [[ $numOfHeads -ne 21 && $numOfTails -ne 21 ]]
-do
-	flipResult=$(( RANDOM%2 ))
-	if [ $flipResult -eq 0 ]
-	then
-		echo "Heads"
-		numOfHeads=$(( $numOfHeads + 1 ))
-	else
-		echo "Tails"
-		numOfTails=$(( $numOfTails + 1 ))
-	fi
-done
+	while [[ $numOfHeads -ne 21 && $numOfTails -ne 21 ]]
+	do
+		flipResult=$(( RANDOM%2 ))
+		resultCounter $flipResult
+	done
 
-if [ $numOfHeads -eq 21 ]
+margin=$( marginCheck $numOfHeads $numOfTails )
+if [ $margin -eq 0 ]
 then
-	winMargin=$(( $numOfHeads - $numOfTails ))
-	echo "Heads won by $winMargin over Tails"
+	while [ $margin -le 2 ]
+	do
+		flipResult=$(( RANDOM % 2 ))
+		resultCounter $flipResult
+		margin=$( marginCheck $numOfHeads $numOfTails )
+	done
+fi
+
+if [ $numOfHeads -gt $numOfTails ]
+then
+	echo "Head Won  $margin over Tails"
 else
-	winMargin=$(( $numOfTails - $numOfHeads ))
-	echo "Tails won by $winMargin over Heads"
+	echo "Tail Won $margin over Heads"
 fi
