e6ca20f8bf7abda535f5e508dc99cecf55a5f09d
anish <anishsalgia1997@gmail.com>
2020-12-11 17:57:58 +0530

    Refactor: Added code to get number of head and tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,14 +1,26 @@
 #!/bin/bash -x
 echo "Welcome to FlipCoin Simulator"
+
+numOfHeads=0
+numOfTails=0
+
+echo "Well To Flip Coin Simulator"
+
 read -p "Do you want to flip a coin (y/n) : " per
 
-if [ $per == "y" ]
-then
+while [ $per == "y" ]
+do
 	flipResult=$(( RANDOM%2 ))
 	if [ $flipResult -eq 0 ]
 	then
 		echo "Heads"
+		numOfHeads=$(( $numOfHeads + 1 ))
 	else
 		echo "Tails"
+		numOfTails=$(( $numOfTails + 1 ))
 	fi
-fi
+	read -p "Do you want to flip a coin again (y/n) : " per
+done
+
+echo "Total number of Heads : " $noOfHeads
+echo "Total number of Tails : " $noOfTails
