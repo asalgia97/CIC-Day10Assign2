2b3b95c6e37fa042de282bc42b8a0a4b11f01edb
anish <anishsalgia1997@gmail.com>
2020-12-11 18:18:23 +0530

    Refactor: Added code to check till one wins number of times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,11 +4,9 @@ echo "Welcome to FlipCoin Simulator"
 numOfHeads=0
 numOfTails=0
 
-echo "Well To Flip Coin Simulator"
+echo "Welcome To Flip Coin Simulator"
 
-read -p "Do you want to flip a coin (y/n) : " per
-
-while [ $per == "y" ]
+while [[ $numOfHeads -ne 21 && $numOfTails -ne 21 ]]
 do
 	flipResult=$(( RANDOM%2 ))
 	if [ $flipResult -eq 0 ]
@@ -19,8 +17,13 @@ do
 		echo "Tails"
 		numOfTails=$(( $numOfTails + 1 ))
 	fi
-	read -p "Do you want to flip a coin again (y/n) : " per
 done
 
-echo "Total number of Heads : " $noOfHeads
-echo "Total number of Tails : " $noOfTails
+if [ $numOfHeads -eq 21 ]
+then
+	winMargin=$(( $numOfHeads - $numOfTails ))
+	echo "Heads won by $winMargin over Tails"
+else
+	winMargin=$(( $numOfTails - $numOfHeads ))
+	echo "Tails won by $winMargin over Heads"
+fi
