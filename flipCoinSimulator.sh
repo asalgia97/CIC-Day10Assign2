7f82c1e65b5f1eeef68e12b9183eec73a09a478d
anish <anishsalgia1997@gmail.com>
2020-12-11 17:50:48 +0530

    ADD: Added code to print head or tail using random

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,14 @@
+#!/bin/bash -x
 echo "Welcome to FlipCoin Simulator"
+read -p "Do you want to flip a coin (y/n) : " per
+
+if [ $per == "y" ]
+then
+	flipResult=$(( RANDOM%2 ))
+	if [ $flipResult -eq 0 ]
+	then
+		echo "Heads"
+	else
+		echo "Tails"
+	fi
+fi
